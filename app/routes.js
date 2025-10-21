97a3c54787d03fd86f86a185b56333b97f43bd26
WaficMikati <wrmikati@proton.me>
2025-10-20 21:55:17 -0500

    merged

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,9 @@
 export default [
   { path: '/', file: 'routes/_index.jsx', showInNav: true, label: "home" },
+  { path: '/contact', file: 'routes/contact.jsx', showInNav: true, label: "contact" },
+  { path: '/test', file: 'routes/test.jsx', showInNav: true, label: "test" },
+  { path: '/users', file: 'routes/users.jsx', showInNav: false },
+  { path: '/:name', file: 'routes/$name.jsx', showInNav: false },
   { 
     path: '/courses/', 
     file: 'routes/courses.jsx', 
@@ -8,9 +12,5 @@ export default [
     children: [
       { path: ':tab', file: 'routes/courses.$tab.jsx' }
     ]
-  },
-  { path: '/contact', file: 'routes/contact.jsx', showInNav: true, label: "contact" },
-  { path: '/test', file: 'routes/test.jsx', showInNav: true, label: "test" },
-  { path: '/users', file: 'routes/users.jsx', showInNav: false },
-  { path: '/:name', file: 'routes/$name.jsx', showInNav: false }
+  }
 ]
